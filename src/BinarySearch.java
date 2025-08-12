21643c4ae82e7598bb35d187dadf2be3e8bff73d
Poorvi Vijayvargiya <66432732+poorvi176@users.noreply.github.com>
2025-08-12 23:28:35 +0530

    Create BinarySearch.java

---
+++ src/BinarySearch.java
@@ -0,0 +1,18 @@
+public class BinarySearch {
+  public static int serach(int[] arr, int k) {
+    int n = arr.length;
+    int low = 0;
+    int high = n-1;
+    int mid;
+
+    while(low<=high) {
+      mid = (low+high)/2;
+      if(arr[mid] == k) {
+        return mid;
+  } else if(arr[mid] < k) {
+        low = mid + 1;
+      } else {
+        high = mid - 1
+      }
+      return -1;
+}
